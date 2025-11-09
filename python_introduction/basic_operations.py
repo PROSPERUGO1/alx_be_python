3d49b838d1965f35a0f770eff308c5eb01ea4dfa
ugochukwu mbagwu <ugochukwumbagwu@gmail.com>
2025-11-09 10:48:09 -0900

    result for addition,subtraction and multiplication

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,18 @@
+number1 = 10
+number2 =5
+result = number1 + number2
+print("Addition of", number1, "and", number2, "is", result)
+
+
+number1 = 10
+number2 =5
+result = number1 - number2
+print("subtraction of", number1, "and", number2, "is", result)
+
+
+number1 = 10
+number2 =5
+result = number1 * number2
+print("multiplication of", number1, "and", number2, "is", result)
+
+
