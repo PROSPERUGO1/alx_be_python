47ba9e0a576c57ca3f08ad4aa9184b5ee22d61a6
ugochukwu mbagwu <ugochukwumbagwu@gmail.com>
2025-11-09 18:42:15 -0900

    rearranged for addition,subtraction and multiplication

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,18 +1,16 @@
 number1 = 10
-number2 =5
+number2 = 5
 result = number1 + number2
 print("Addition of", number1, "and", number2, "is", result)
 
 
 number1 = 10
-number2 =5
+number2 = 5
 result = number1 - number2
-print("subtraction of", number1, "and", number2, "is", result)
+print("Subtraction of", number1, "and", number2, "is", result)
 
 
 number1 = 10
-number2 =5
+number2 = 5
 result = number1 * number2
-print("multiplication of", number1, "and", number2, "is", result)
-
-
+print("Multiplication of", number1, "and", number2, "is", result)
