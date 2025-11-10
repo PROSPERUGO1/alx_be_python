9d584d1c732074bce0c947e398b28a6893c90647
ugochukwu mbagwu <ugochukwumbagwu@gmail.com>
2025-11-09 21:57:19 -0900

    rectified

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,9 @@
-principle = 1000
-rate = 0.05
-time = 3
+# A python scrip that calculates the simple interest earned on an investment over a period of time. The formula for simple interest is (I = P * R * T) , where:
+
+principle = 1000 # initial amount of $1000
+rate = 0.05 # 5% annual interest rate
+time = 3 # 3 years
+
+# Calculate the simple interest using the formular
 interest = principle * rate * time
 print("The simple interest is:", interest)
